a8bbaf4a4e57654c50baccbe296380ea6a3c1e3e
hodeethelion <119830726+hodeethelion@users.noreply.github.com>
2023-03-21 19:59:15 +0900

    [Silver III] Title: 바이러스, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,24 +1,22 @@
 import sys
-sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
 n_computer = int(input())
-n_link = int(input())
-graph = [[] for i in range(n_computer+1)]
-
-for _ in range(n_link):
+n_line = int(input())
+graph = [[] for _ in range(n_computer+1)]
+for _ in range(n_line):
     a, b = map(int, input().split())
     graph[a].append(b)
     graph[b].append(a)
 
-for item in graph:
-    item.sort()
+visited = [False] * (n_computer+1)
 
-visited = [False] * (n_computer + 1)
-visit_computer = []
+infected = []
 def dfs(graph, start, visited):
     visited[start] = True
-    visit_computer.append(start)
     for item in graph[start]:
-        if not visited[item]:
+        if visited[item] == False:
+            infected.append(item)
+            # visited[item] = True
             dfs(graph, item, visited)
 dfs(graph,1,visited)
-print(len(visit_computer)-1)
+print(len(infected))
