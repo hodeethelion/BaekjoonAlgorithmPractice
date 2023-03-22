06e0f0a871ba74d5bd818f9a2c5413037281c493
hodeethelion <119830726+hodeethelion@users.noreply.github.com>
2023-03-22 14:17:02 +0900

    [Gold IV] Title: 이분 그래프, Time: 1248 ms, Memory: 53344 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.py
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.py
@@ -1,36 +1,45 @@
 import sys
-sys.setrecursionlimit(1000000)
+sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
+n_test = int(input())
 
-K = int(input())
 
-
+# 데이터 저장을 visited에다가 하는 방식
 def dfs(start, group):
-    visited[start] = group  # 해당 정점의 group 설정(1,-1)
+    visited[start] = group
+    for item in graph[start]:
+        if visited[item] == group:
+            return False
+        elif visited[item] == -group:
+            continue
+        else:
+            next_stage = dfs(item, -group)
+            if next_stage == False:
+                return False
+            else:
+                continue
+    return True
 
-    for i in graph[start]:
-        if not visited[i]:  # 만약 방문하지 않았다면
-            a = dfs(i, -group)  # 그룹값을 -1로 주어 dfs를 돈다.
-            if not a:  # 만약 a가 false가 나왔다면
-                return False  # False를 리턴
-        elif visited[i] == visited[start]:  # 만약 현재 정점과 연결된 정점의 그룹값이 같다면
-            return False  # False를 리턴
-    return True  # 그외의 경우는 True를 리턴
-
-
-for _ in range(K):
-    V, E = map(int, input().split())
-    graph = [[] for _ in range(V + 1)]  # 빈 그래프 생성
-    visited = [False] * (V + 1)  # 방문한 정점 체크
-    for _ in range(E):
+for _ in range(n_test):
+    n_vertex, n_line = map(int, input().split())
+    graph = [[] for _ in range(n_vertex+1)]
+    for _ in range(n_line):
         a, b = map(int, input().split())
         graph[a].append(b)
         graph[b].append(a)
+    visited = [0] * (n_vertex+1)
+    ans = True
 
-    for i in range(1, V + 1):
-        if not visited[i]:  # 방문한 정점이 아니면, dfs 수행
-            result = dfs(i, 1)
-            if not result:  # 만약 result가 False가 나왔다면 더이상 수행할 필요가 없으므로
-                break  # break
+    for sample in range(1, n_vertex+1):
+        if visited[sample] == 0:
+            ans = dfs(sample, 1)
+            if ans == False:
+                break
+            else:
+                continue
+    if ans== False: 
+        print('NO')
+    else: 
+        print('YES')
 
-    print("YES" if result else "NO")
+        
