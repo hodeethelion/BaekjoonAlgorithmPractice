dae0a74d75bf1a59cb37bc98772e79b80a0d8557
hodeethelion <119830726+hodeethelion@users.noreply.github.com>
2023-05-04 13:00:06 +0900

    [level 2] Title: 타겟 넘버, Time: 2.47 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,19 +1,4 @@
 def solution(numbers, target):
-    # idx = 0
-    target_n = len(numbers)
-    ans = []
-    
-    def dfs(index, result):
-        
-        if index == target_n:
-            if result == target:
-                ans.append(1)
-                return
-            else:
-                return
-        else:
-            dfs(index + 1, result + numbers[index])
-            dfs(index + 1, result - numbers[index])
-    
-    dfs(0, 0)
-    return sum(ans)
+    print(numbers)
+    print(target)
+    return 
