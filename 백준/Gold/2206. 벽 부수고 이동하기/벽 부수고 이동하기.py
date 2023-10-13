da67738e0cdb4e897dee1997e87547f62da213fa
hodee <119830726+hodeethelion@users.noreply.github.com>
2023-10-13 23:10:40 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 5948 ms, Memory: 191992 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -1,36 +1,36 @@
 from collections import deque
-n, m = map(int, input().split())
+r, c = map(int, input().split())
 graph = []
-for _ in range(n):
+for _ in range(r):
     graph.append(list(map(int, list(input().rstrip()))))
-
-visited = [[[0, 0] for _ in range(m)] for _ in range(n)]
+visited = [[[0, 0] for _ in range(c)] for _ in range(r)]
 visited[0][0][0] = 1
 
-# 상 하 좌 우
-drs = [0, 0, 1, -1]
-dcs = [1, -1, 0, 0]
+dcs = [-1, 1, 0, 0]
+drs = [0, 0, -1, 1]
 
-def in_range(row, col):
-    return 0 <= row and row < n and 0 <= col and col < m
+def in_range(x, y):
+    return 0 <= x and x < r and 0<= y and y < c
 
-def bfs(r, c, z):
+def bfs():
     q = deque()
-    q.append([r, c, z])
+    q.append([0, 0, 0])
+
     while q:
-        row, col, z = q.popleft()
-        if row == n - 1 and col == m - 1:
-            return visited[row][col][z]
-        for dr, dc in zip(drs, dcs):
-            nr = dr + row
-            nc = dc + col
-            # 1. 이동할 곳이 벽인 경우
-            if in_range(nr, nc) and graph[nr][nc] == 1 and z == 0:
-                visited[nr][nc][1] = visited[row][col][0] + 1
+        c_r, c_c, z = q.popleft()
+
+        if c_r == r - 1 and c_c == c - 1:
+            return visited[c_r][c_c][z]
+
+        for dc, dr in zip(dcs, drs):
+            nc, nr = c_c + dc, c_r + dr
+            # 1. 벽을 만나서 부셔야 하는 상황이라면
+            if in_range(nr, nc) and graph[nr][nc] == 1 and visited[nr][nc][z] == 0 and z == 0:
+                visited[nr][nc][1] = visited[c_r][c_c][0] + 1
                 q.append([nr, nc, 1])
-            # 2. 이동할 곳이 벽이 아닌 경우
-            elif in_range(nr, nc) and graph[nr][nc] == 0 and visited[nr][nc][z] == 0:
-                visited[nr][nc][z] = visited[row][col][z] + 1
+            # 2. 벽을 안만나서 그냥 지나갈 수 있는 상황이라면
+            elif in_range(nr, nc) and visited[nr][nc][z] == 0 and graph[nr][nc] == 0:
+                visited[nr][nc][z] = visited[c_r][c_c][z] + 1
                 q.append([nr, nc, z])
     return -1
-print(bfs(0, 0, 0))
+print(bfs())
