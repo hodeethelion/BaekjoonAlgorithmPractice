80623615e98b7e2a3aa4dfc1edbd5bc0163a4bae
hodee <119830726+hodeethelion@users.noreply.github.com>
2023-10-13 17:02:29 +0900

    [Silver I] Title: 단지번호붙이기, Time: 64 ms, Memory: 34096 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,46 +1,41 @@
-import sys
 from collections import deque
-input = sys.stdin.readline
-n = int(input())
-graph = []
-for _ in range(n):
-    graph.append(list(map(int, input().strip())))
-# for i in graph:
-#     print(i)
 
-dr= [-1,1,0,0]
-dc= [0,0,-1,1]
-visited = [[0]* n for _ in range(n)]
-# print(visited)
-count = 0
+N = int(input())
+map_graph = []
+visit_graph = []
+for _ in range(N):
+    map_graph.append(list(map(int, list(input().rstrip()))))
+    visit_graph.append([False for _ in range(N)])
 
-def bfs(start_r, start_c):
-    global count
+drs = [-1, 1, 0, 0]
+dcs = [0, 0, -1, 1]
+def in_range(i, j):
+    return 0 <= i and i < N and 0 <= j and j < N
+
+def can_go(i, j):
+    return in_range(i, j) and visit_graph[i][j] == False and map_graph[i][j] == 1
+
+def bfs(i, j):
     q = deque()
-    q.append((start_r, start_c))
-    visited[start_r][start_c] = 1
-    count += 1
+    q.append([i, j])
+    count = 0
     while q:
-        cur_r, cur_c = q.popleft()
-        for i in range(4):
-            next_r = cur_r + dr[i]
-            next_c = cur_c + dc[i]
-            if 0<= next_r <n and \
-               0<= next_c <n and \
-               visited[next_r][next_c] == 0 and\
-               graph[next_r][next_c] == 1:
-                count+=1
-                # print(count)
-                visited[next_r][next_c] = 1 
-                q.append((next_r, next_c))
+        row, col = q.popleft()
+        if can_go(row, col):
+            count += 1
+            visit_graph[row][col] = True
+            for dr, dc in zip(drs, dcs):
+                if can_go(dr + row, dc + col):
+                    q.append([dr+row, dc + col])
     return count
-ans_list= []
-for i in range(n):
-    for j in range(n):
-        if visited[i][j]== 0 and graph[i][j] ==1:
-            count = 0
-            ans_list.append(bfs(i,j))
-# print(ans_list)
+
+ans_list = []
+
+for r in range(N):
+    for c in range(N):
+        if can_go(r, c):
+            ans = bfs(r, c)
+            ans_list.append(ans)
 ans_list.sort()
 print(len(ans_list))
 for i in ans_list:
